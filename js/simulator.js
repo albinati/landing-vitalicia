0afd7d3c4d85ab65d35757dcca0a1723b1a3843a
Rodrigo Mendonça <rodam@metec.com.br>
2020-05-06 19:54:32 -0300

    add simultation

--- js/simulator.js
+++ js/simulator.js
@@ -1,3 +1,44 @@
+var email_regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
+
+function validateEmail() {
+    if (!email_regex.test($("#email_quote").val())) {
+        $("#email_label").html("Email inválido!");
+        $('#email_quote').trigger('focus');
+        return false
+    } else {
+        $("#email_label").html("");
+        return true
+    }
+}
+
+function quote() {
+
+    if (!validateEmail()) {
+        return false;
+    }
+
+    quote_data = {
+        "email": $('#email_quote').val(),
+        "times": $('#parcela').val(),
+        "need": $('#need').val(),
+        "phone": $("#phone_quote").val()
+    }
+    console.log(quote_data);
+    var headers = { "content-type": "application/json; charset=UTF-8" }
+
+    var xhr = new XMLHttpRequest();
+    var myHeaders = new Headers();
+    myHeaders.append("content-type", "application/json; charset=UTF-8")
+    fetch("https://b2y82q8aa5.execute-api.us-east-2.amazonaws.com", {
+        headers: myHeaders,
+        data: quote_data
+    }).then(function(response) {
+        console.log(response.json());
+    }).then(function(json) {
+        console.log(json);
+    });
+}
+
 function get_value(key) {
     var values = {
         "": [0, ""],
@@ -14,16 +55,14 @@ function get_value(key) {
     return values[key];
 }
 
-$('#combobox').on('change', function(e) {
+$('#need').on('change', function(e) {
     $('#valor_procedimento').text("");
     $('#legenda_valor').text("");
     $('#valor').val(0);
-    $('step2').hide();
     $('#valor_procedimento').text(get_value(this.value)[1]);
     $('#valor').val(get_value(this.value)[0]);
     set_valor();
     $('#legenda_valor').text("-  valor global de referência");
-    $('step2').show();
 });
 
 var slider = document.getElementById("parcela");
@@ -35,17 +74,24 @@ slider.oninput = function() {
     parcelas = this.value
     output.innerHTML = parcelas + "x";
     set_valor();
-
 }
 
 function set_valor() {
     parcelas = $('#parcela').val();
-    valor = $('#valor').val() * 1.05;
-    valor_parcela = valor / parcelas;
-    valor_final = (valor_parcela).toLocaleString("pt-BR", { style: "currency", currency: "BRL", minimumFractionDigits: 2 });
-    $('#legenda_valor_final').text(valor_final);
+    valor = $('#valor').val();
+    taxa_juros = 0.02;
+    valor_final = valor * Math.pow((1 + taxa_juros), parcelas);
+    valor_parcela = valor_final / parcelas;
+    cada_parcela = (valor_parcela).toLocaleString("pt-BR", { style: "currency", currency: "BRL", minimumFractionDigits: 2 });
+    $('#legenda_valor_final').text(cada_parcela);
 }
 
 $(document).ready(function() {
     $('.combobox').combobox()
 });
+
+$('#cotacaoModal').on('shown.bs.modal', function() {
+    $('#email_quote').trigger('focus')
+})
+
+$("#email_quote").blur(validateEmail)
